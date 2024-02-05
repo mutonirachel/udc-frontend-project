45ad625c4e21408def9ab41375a8ce15359a6905
mutonirachel <109520133+mutonirachel@users.noreply.github.com>
2024-02-05 18:39:55 +0200

    Update NewServices.jsx

--- src/components/NewServices.jsx
+++ src/components/NewServices.jsx
@@ -1,41 +1,40 @@
 import React from "react";
-import project from "../assets/project.jpg"
-import project7 from "../assets/project7.jpg"
+import project from "../assets/Civil Engineering 2.jpg"
+import project7 from "../assets/Archtecural pics.jpg"
 import AOS from "aos";
 import "aos/dist/aos.css";
 AOS.init();
 
-const NewServices = ({imgDir, title,content}) => {
+const NewServices = ({imgDir, title, content}) => {
     
   return (
-    <section className="lg:px-24 px-16 py-24" id='services' data-aos="fade-up"
-    data-aos-duration={2000}>
+    <section className="lg:px-24 px-6 py-24" id='services' data-aos="fade-up" data-aos-duration={2000}>
 
-        {imgDir === 'right' ? (
-      <div className="flex lg:flex-row flex-col lg:justify-between">
-        
-        <div className="flex flex-col lg:w-[45%] w-full">
-          <h1 className="lg:text-4xl text-2xl text-blue-700 font-medium uppercase">{title}</h1>
-          <p className="text-[15.0385px] text-justify pt-4  text-[#777777]">{content}</p>
-        </div>
-        <div className="lg:w-[45%] w-full border-4 border-blue-700 h-[23vh] lg:h-[49vh]">
-          <img src={project7} className="ml-[-40px] mt-[-40px] object-cover" />
-        </div>
+  {imgDir === 'right' ? (
+    <div className="flex flex-col lg:flex-row lg:justify-around">
+      <div className="lg:w-1/2 my-16">
+        <h1 className="lg:text-4xl text-2xl text-blue-700 font-medium uppercase">{title}</h1>
+        <p className="text-[18] text-justify pt-4 text-[#777777]">{content}</p>
       </div>
-      ) : 
-
-      <div className="flex lg:flex-row flex-col lg:justify-between">
-        <div className="lg:w-[45%] w-full border-4 border-blue-700 h-[23vh] lg:h-[49vh]">
-          <img src={project} className="lg:ml-[-40px] ml-[-20px] mt-[-40px] object-cover" />
-        </div>
-        <div className="flex flex-col lg:w-[45%] w-full">
-          <h1 className="lg:text-4xl text-2xl my-8 lg:mt-0 text-blue-700 font-medium uppercase">{title}</h1>
-          <p className="text-[#777777] text-justify pt-4 text-[15.0385px]">{content}</p>
-        </div>
+      <div className="w-1/2">
+      <img src={project7}
+  className="ml-0 md:ml-12 w-full mt-[-40px] lg:mt-0 object-cover h-[50vh] lg:h-[50vh]" alt="Project Image"/>
       </div>
+    </div>
+  ) : (
+    <div className="flex flex-col lg:flex-row lg:justify-around">
+      <div className="lg:w-1/2">
+        <img src={project} className="lg:ml-[-40px] ml-[-20px] mt-[-40px] lg:mt-0 object-cover w-full" alt="Project Image" />
+      </div>
+      <div className="lg:w-1/2 my-16">
+        <h1 className="lg:text-4xl text-2xl my-8 lg:mt-0 text-blue-700 font-medium uppercase">{title}</h1>
+        <p className="text-[#777777] text-justify pt-4 text-[18x]">{content}</p>
+      </div>
+    </div>
+  )}
 
-}
-    </section>
+</section>
+
   );
 };
 
